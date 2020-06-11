47a1b36fa95b80c725c66186bc41f4222194eac5
Matt Braddock <matt.braddock@gmail.com>
2020-06-10 20:39:13 -0400

    interface update

--- main/preload.js
+++ main/preload.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
     let PortOptions = new DocumentFragment()
     SerialPort.list().then(ports => {
       ports.forEach(port => {
-        console.log(port) //checking if it works
         if (port.manufacturer != undefined) {
           const PortOptionElement = document.createElement('option')
           PortOptionElement.setAttribute('value', port.path)
@@ -13,6 +12,10 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       })
       document.getElementById("port-select").appendChild(PortOptions)
+      if (document.getElementById("port-select").length === 0) {
+        document.getElementById("start-button").style.display = "none"
+        document.getElementById("scan-button").style.display = "blocK"
+      }
     })
 
     document.getElementById("version-number").innerText = process.env.npm_package_version
--- public/interface.js
+++ public/interface.js
@@ -1,3 +1,5 @@
+const SerialPort = require('serialport')
+
 function disableAltConsole() {
   if (document.getElementById('console-select').value === 'n64' || document.getElementById('console-select').value === 'gcn') {
     document.getElementById('color-select').options.namedItem('console-alt').disabled = true
@@ -5,4 +7,23 @@ function disableAltConsole() {
   } else {
     document.getElementById('color-select').options.namedItem('console-alt').disabled = false
   }
+}
+
+function scanPorts() {
+  let PortOptions = new DocumentFragment()
+    SerialPort.list().then(ports => {
+      ports.forEach(port => {
+        if (port.manufacturer != undefined) {
+          const PortOptionElement = document.createElement('option')
+          PortOptionElement.setAttribute('value', port.path)
+          PortOptionElement.innerText = port.path + ' - ' + port.manufacturer
+          PortOptions.appendChild(PortOptionElement)
+        }
+      })
+      document.getElementById("port-select").appendChild(PortOptions)
+      if (document.getElementById("port-select").length != 0) {
+        document.getElementById("scan-button").style.display = "none"
+        document.getElementById("start-button").style.display = "blocK"
+      }
+    })
 }
