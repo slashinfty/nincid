4392450b3a1d9a908b9b7a29fd9d191ccc2348cf
slashinfty <matt.braddock@gmail.com>
2020-06-11 15:40:15 -0400

    before buttons

--- public/display_interface.js
+++ public/display_interface.js
@@ -1,26 +1,31 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 var activePort;
 var skinPath;
 
 function loadDisplay() {
-    // Get settings, find JSON, load background.
-    document.getElementById("settings-wrapper").style.display = "none"
-    const consoleName = document.getElementById("console-select").value
-    const color = document.getElementById("color-select").value
-    skinPath = path.join(__dirname, "../static/skins/", consoleName)
-    const skinJson = require(`${skinPath}/skin.json`)
-    const background = skinJson.background.find(skin => skin.name === color).image
-    document.body.style.backgroundImage = "url(" + skinPath + "/" + background + ")"
-    
-    // Resize window based on height/width in JSON.
-    const win = require("electron").remote.BrowserWindow.getFocusedWindow();
-    win.setSize(skinJson.width, skinJson.height)
+  // Get settings, find JSON, load background.
+  document.getElementById("settings-wrapper").style.display = "none"
+  const consoleName = document.getElementById("console-select").value
+  const color = document.getElementById("color-select").value
+  skinPath = path.join(__dirname, "../static/skins/", consoleName)
+  const skinJson = require(`${skinPath}/skin.json`)
+  const background = skinJson.background.find(skin => skin.name === color).image
+  document.body.style.backgroundImage = "url(" + skinPath + "/" + background + ")"
+  
+  // Resize window based on height/width in JSON.
+  const win = require("electron").remote.BrowserWindow.getFocusedWindow();
+  win.setSize(skinJson.width, skinJson.height)
 
-    activePort = new SerialPort(document.getElementById("port-select").value, {
-      baudRate: 115200
-    });
-    activePort.on('data', function (data) {
-      console.log('Data:', data)
-    })
+  // Set buttons in place
+
+  // Open port and start
+  activePort = new SerialPort(document.getElementById("port-select").value, {
+    baudRate: 115200
+  });
+  activePort.on('data', data => {
+    if(data[0] !== 0) console.log("down");
+    if(data[3] !== 0) console.log("A");
+  })
 }
