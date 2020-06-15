0df713e40f6dd872d50ce7cb4bce5333a35f1001
slashinfty <matt.braddock@gmail.com>
2020-06-14 20:34:18 -0400

    stick wip

--- public/display_interface.js
+++ public/display_interface.js
@@ -3,6 +3,73 @@ const ByteLength = require('@serialport/parser-byte-length')
 var activePort;
 var skinPath;
 
+const consoleObject = [
+  {
+    "name": "nes",
+    "length": 9,
+    "buttons": []
+  },
+  {
+    "name": "snes",
+    "length": 17,
+    "buttons": [
+      {"button": "b", "input": 0},
+      {"button": "y", "input": 1},
+      {"button": "select", "input": 2},
+      {"button": "start", "input": 3},
+      {"button": "up", "input": 4},
+      {"button": "down", "input": 5},
+      {"button": "left", "input": 6},
+      {"button": "right", "input": 7},
+      {"button": "a", "input": 8},
+      {"button": "x", "input": 9},
+      {"button": "l", "input": 10},
+      {"button": "r", "input": 11}
+    ]
+  },
+  {
+    "name": "n64",
+    "length": 33,
+    "buttons": [
+      {"button": "a", "input": 0},
+      {"button": "b", "input": 1},
+      {"button": "z", "input": 2},
+      {"button": "start", "input": 3},
+      {"button": "up", "input": 4},
+      {"button": "down", "input": 5},
+      {"button": "left", "input": 6},
+      {"button": "right", "input": 7},
+      {"button": "l", "input": 10},
+      {"button": "r", "input": 11},
+      {"button": "cup", "input": 12},
+      {"button": "cdown", "input": 13},
+      {"button": "cleft", "input": 14},
+      {"button": "cright", "input": 15}
+    ],
+    "sticks": [
+      {"stick": "stick", "xinput": 16, "yinput": 24}
+    ]
+  },
+  {
+    "name": "gcn",
+    "buttons": []
+  },
+  {
+    "name": "sgb",
+    "length": 17,
+    "buttons": [
+      {"button": "a", "input": 0},
+      {"button": "b", "input": 1},
+      {"button": "select", "input": 2},
+      {"button": "start", "input": 3},
+      {"button": "up", "input": 4},
+      {"button": "down", "input": 5},
+      {"button": "left", "input": 6},
+      {"button": "right", "input": 7}
+    ]
+  }
+]
+
 function loadDisplay() {
   // Get settings, find JSON, load background.
   document.getElementById("settings-wrapper").style.display = "none"
@@ -22,7 +89,7 @@ function loadDisplay() {
   const ButtonHolder = new DocumentFragment()
   skinJson.buttons.forEach(button => {
     const ButtonElement = document.createElement("img")
-    const buttonID = button.hasOwnProperty("name") ? button.name : button.xname.slice(0, -2)
+    const buttonID = button.name // fix gcn json
     ButtonElement.setAttribute("id", buttonID)
     ButtonElement.setAttribute("src", skinPath + "/" + button.image)
     const widthHeight = button.hasOwnProperty("width") ? "height:" + button.height + "px;width:" + button.width + "px;" : ""
@@ -36,86 +103,46 @@ function loadDisplay() {
     baudRate: 115200
   });
 
-  controllerDisplay(consoleName)
-}
-
-function controllerDisplay(consol) {
-  const consoleObject = [
-    {
-      "name": "nes",
-      "length": 9,
-      "buttons": []
-    },
-    {
-      "name": "snes",
-      "length": 17,
-      "buttons": [
-        {"button": "b", "input": 0},
-        {"button": "y", "input": 1},
-        {"button": "select", "input": 2},
-        {"button": "start", "input": 3},
-        {"button": "up", "input": 4},
-        {"button": "down", "input": 5},
-        {"button": "left", "input": 6},
-        {"button": "right", "input": 7},
-        {"button": "a", "input": 8},
-        {"button": "x", "input": 9},
-        {"button": "l", "input": 10},
-        {"button": "r", "input": 11}
-      ]
-    },
-    {
-      "name": "n64",
-      "length": 33,
-      "buttons": [
-        {"button": "a", "input": 0},
-        {"button": "b", "input": 1},
-        {"button": "z", "input": 2},
-        {"button": "start", "input": 3},
-        {"button": "up", "input": 4},
-        {"button": "down", "input": 5},
-        {"button": "left", "input": 6},
-        {"button": "right", "input": 7},
-        {"button": "l", "input": 10},
-        {"button": "r", "input": 11},
-        {"button": "cup", "input": 12},
-        {"button": "cdown", "input": 13},
-        {"button": "cleft", "input": 14},
-        {"button": "cright", "input": 15}
-      ]
-    },
-    {
-      "name": "gcn",
-      "buttons": []
-    },
-    {
-      "name": "sgb",
-      "buttons": [
-        {"button": "a", "input": 0},
-        {"button": "b", "input": 1},
-        {"button": "select", "input": 2},
-        {"button": "start", "input": 3},
-        {"button": "up", "input": 4},
-        {"button": "down", "input": 5},
-        {"button": "left", "input": 6},
-        {"button": "right", "input": 7}
-      ]
-    }
-  ]
-  
-  const activeConsole = consoleObject.find(obj => obj.name === consol)
+  const activeConsole = consoleObject.find(obj => obj.name === consoleName)
   const consoleButtons = activeConsole.buttons
+  const consoleSticks = activeConsole.hasOwnProperty('sticks') ? activeConsole.sticks : null
   const parser = activePort.pipe(new ByteLength({length: activeConsole.length}))
+
+  /*
+  let stickRead = bitArray => {
+    let byte = 0
+    bitArray.forEach((bit, index) => {
+      if (bit & 0x0F != 0) { byte |= 1 << (7 - index) }
+    })
+    console.log('byte: ' + byte)
+    byte = byte > 127 ? -1 * (256 - byte) : byte
+    console.log('signed: ' + byte)
+    console.log(parseFloat(byte / 128))
+    return parseFloat(byte / 128)
+  }
+  */
+
+  const isHighBit = (bit) => (bit & 0x0F !== 0);
+
+  const stickRead = (bitArray) => {
+    let val = 0
+    for (let i = 1; i < 8; i += 1) { if (isHighBit(bitArray[i])) { val |= 1 << (7 - i) } }
+    return (isHighBit(bitArray[0]) ? (val - 128) : val) / 128;
+  };
+
   parser.on('data', data => {
-    console.log(data);
+    //console.log(data);
+    let offset = data.indexOf(10) + 1
+    if (consoleSticks != null) {
+      consoleSticks.forEach(stk => {
+        let xinput = stk.xinput + offset
+        let yinput = stk.yinput + offset
+        let x = stickRead(data.slice(xinput % data.length, (xinput + 8) % data.length))
+        let y = stickRead(data.slice(yinput % data.length, (yinput + 8) % data.length))
+      })
+    }
     consoleButtons.forEach(but => {
-      document.getElementById(but.button).style.visibility = data[but.input] === 0 ? "hidden" : "visible"
+      document.getElementById(but.button).style.visibility = data[(but.input + offset) % data.length] === 0 ? "hidden" : "visible"
     })
   })
-  //activePort.on('data', data => {
-    //console.log(data);
-    //activeConsole.forEach(but => {
-      //document.getElementById(but.button).style.visibility = data[but.input] === 0 ? "hidden" : "visible"
-    //})
-  //})
 }
